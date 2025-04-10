d2feb521cbfcdd49b2f7ced7cb9ae8395a04483d
Gyana prakash Khandual <gyanaprakashkhnadual@gmail.com>
2025-04-10 15:43:13 +0530

    Procurement Load Testing codes

---
+++ Load/Procurement.js
@@ -0,0 +1,30 @@
+import http from "k6/http";
+import { check } from "k6";
+
+const testType = __ENV.TEST_TYPE || "10";
+
+const validTestTypes = [10, 100, 200, 300, 400, 500, 600, 700, 800, 900, 1000];
+
+if (!validTestTypes.includes(Number(testType))) {
+    console.error(`Invalid TEST_TYPE: ${testType}. Please use one of ${validTestTypes.join(", ")}.`);
+    throw new Error("Invalid TEST_TYPE");
+}
+
+const vus = Number(testType);
+const iterations = Number(testType);
+
+export const options = {
+    vus,
+    iterations,
+};
+
+export function Loginpage() {
+    const response = http.get('https://staging.pislinfra.com/#/procurement');
+    check(response, {
+        'is status code 200': (r) => r.status === 200,
+    });
+}
+
+export default function () {
+    Loginpage();
+}
